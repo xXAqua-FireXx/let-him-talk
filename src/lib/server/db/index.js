0dc7596d8e26f65ac8a814ee3a6cabb0af5f7af8
Adnan Ahmed <adnan223691@gmail.com>
2025-08-23 17:52:13 +0600

    fixed env variables

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -3,8 +3,8 @@ import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.NETLIFY_DATABASE_URL) throw new Error('NETLIFY_DATABASE_URL is not set');
 
-const client = neon(env.DATABASE_URL);
+const client = neon(env.NETLIFY_DATABASE_URL);
 
 export const db = drizzle(client, { schema });
